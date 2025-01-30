1d69238cc46b320cd924fc128a6e63efafd3101d
Giovana Oliveira <gigioliveirafju@gmail.com>
2025-01-30 19:48:57 -0300

    Displays the footer

--- scripts.js
+++ scripts.js
@@ -7,11 +7,12 @@ const GBP = 6.08
 const form = document.querySelector("form")
 const amount = document.getElementById("amount")
 const currency = document.getElementById("currency")
+const footer = document.querySelector("main footer")
 
 
 // Manipulando o input amount para receber somente números.
 amount.addEventListener("input", () => {
-  
+
   const hasCharactersRegex = /\D+/g
   amount.value = amount.value.replace(hasCharactersRegex, "")
 })
@@ -21,7 +22,7 @@ form.onsubmit = (event) => {
   event.preventDefault()
 
   // console.log(currency.value)
-  switch(currency.value){
+  switch (currency.value) {
     case "USD":
       convertCurrency(amount.value, USD, "US$")
       break
@@ -36,9 +37,15 @@ form.onsubmit = (event) => {
 
 // Função para converter a moeda.
 
-function convertCurrency(amount, price, symbol){
-  console.log(amount, price, symbol)
+function convertCurrency(amount, price, symbol) {
+  try {
+    // Aplica a classe que exibe o footer para mostrar o resultado.
+    footer.classList.add("show-result")
+  } catch (error) {
+    // Remove a classe do footer removendo ele da tela.
+    footer.classList.remove("show-result")
+
+    console.log(error)
+    alert("Não foi possível converter. Tente novamente mais tarde")
+  }
 }
-
-
-
