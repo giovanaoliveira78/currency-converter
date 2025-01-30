fdc1070999e0eb04382c6213e608537d16d7d18b
Giovana Oliveira <gigioliveirafju@gmail.com>
2025-01-30 17:32:47 -0300

    retrieves the currency selected by the user

--- scripts.js
+++ scripts.js
@@ -1,4 +1,8 @@
+// Obtendo os elementos do formulário
+const form = document.querySelector("form")
 const amount = document.getElementById("amount")
+const currency = document.getElementById("currency")
+
 
 // Manipulando o input amount para receber somente números.
 amount.addEventListener("input", () => {
@@ -6,3 +10,10 @@ amount.addEventListener("input", () => {
   const hasCharactersRegex = /\D+/g
   amount.value = amount.value.replace(hasCharactersRegex, "")
 })
+
+// Captando o evento de submit do formulário.
+form.onsubmit = (event) => {
+  event.preventDefault()
+
+  console.log(currency.value)
+}
