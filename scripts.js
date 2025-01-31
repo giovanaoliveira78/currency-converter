3528ad54b45b7aba91911829757f95c0484d80f2
Giovana Oliveira <gigioliveirafju@gmail.com>
2025-01-31 17:14:10 -0300

    exibe a cotacao da moeda

--- scripts.js
+++ scripts.js
@@ -8,6 +8,7 @@ const form = document.querySelector("form")
 const amount = document.getElementById("amount")
 const currency = document.getElementById("currency")
 const footer = document.querySelector("main footer")
+const description = document.getElementById("description")
 
 
 // Manipulando o input amount para receber somente números.
@@ -39,6 +40,8 @@ form.onsubmit = (event) => {
 
 function convertCurrency(amount, price, symbol) {
   try {
+    description.textContent = `${symbol} 1 = ${price}`
+
     // Aplica a classe que exibe o footer para mostrar o resultado.
     footer.classList.add("show-result")
   } catch (error) {
@@ -49,3 +52,8 @@ function convertCurrency(amount, price, symbol) {
     alert("Não foi possível converter. Tente novamente mais tarde")
   }
 }
+
+
+
+
+
