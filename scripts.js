cefdd63596abb7a91c63c859f7df4ffb39057a90
Giovana Oliveira <gigioliveirafju@gmail.com>
2025-01-31 18:44:33 -0300

    formata o total

--- scripts.js
+++ scripts.js
@@ -46,8 +46,16 @@ function convertCurrency(amount, price, symbol) {
     // Calcula o total
     let total = amount * price
 
+    // Verifica se o resultado não é um número
+    if(isNaN(total)){
+      return alert ("Por favor, digite o valor corretamente")
+    }
+
+    // Formata o valor total
+    total = formatCurrencyBRL(total).replace("R$", "")
+
     // Exibe o resultado do total
-    result.textContent = total
+    result.textContent = `${total} Reais`
 
     // Aplica a classe que exibe o footer para mostrar o resultado.
     footer.classList.add("show-result")
